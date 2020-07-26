4449d7c9871dc2b65df2c6b97be561ad49a81ff9
Inês Carvalho <imcarvalho88@gmail.com>
2020-07-26 22:31:53 +0200

    File renaming fail

---
+++ src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import SEO from '../components/Seo';
+import { Location } from '../entities/Location';
+import { SiteMetadata } from '../entities/SiteMetadata';
+
+export default function NotFoundPage(props: {
+  location: Location;
+  data: { site: { siteMetadata: SiteMetadata } };
+}) {
+  return (
+    <Layout
+      location={props.location}
+      siteMetadata={props.data.site.siteMetadata}
+    >
+      <SEO title="404: Not Found" />
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  );
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      ...SiteMetadataFragment
+    }
+  }
+`;
