a859501e4f0fd31aa13535ba303ed54621eb9f5d
Inês Carvalho <>
2020-07-28 15:51:09 +0200

    SEO in typescript

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,16 +1,22 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
-import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
-function SEO({ description, lang, meta, keywords, title }) {
+type Props = {
+  description: string;
+  lang: string;
+  meta: any[];
+  keywords: string[];
+  title: string;
+};
+
+export default function SEO({
+  description,
+  lang,
+  meta,
+  keywords,
+  title,
+}: Props) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -87,13 +93,3 @@ SEO.defaultProps = {
   keywords: [],
   description: ``,
 };
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired,
-};
-
-export default SEO;
