8af4d531cb40711b7a7d0c58528b32b6d9d2af4f
Inês Carvalho <4671109+imcarvalho@users.noreply.github.com>
2020-09-01 18:17:10 +0100

    One more time 🎶

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,7 +8,7 @@ export default function NotFoundPage(props: { location: Location }) {
   return (
     <Layout location={props.location}>
       <SEO title="404: Not Found" />
-      <Flex flexDirection="column" justifyContent="center" width={1}>
+      <Flex flexDirection="column" alignItems="center" width={1}>
         <h2>Not Found</h2>
         <p>RIP ⚰️</p>
         <p>
